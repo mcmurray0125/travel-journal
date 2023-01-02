c5f682309fdb8790777a2e26242b564405bb88e8
Michael Murray <mcmurray0125@gmail.com>
2023-01-02 01:11:50 -0600

    finished travel-journal

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,23 @@ import Navbar from "./components/Navbar"
 import data from './data'
 
 function App() {
+  //Render a HR unless it is the last in the array.
+  function renderHR(items, index) {
+    if (index !== items.length - 1) {
+      return <hr />;
+    }
+    return null;
+  }
 
-  const cardElements = data.map(item => {
+  const cardElements = data.map((item,index) => {
     return (
-      <Card
-          key={item.id}
-          item={item}
-          />
+      <div key={item.id}>
+        <Card
+            key={item.id}
+            item={item}
+            />
+            {renderHR(data, index)}
+      </div>
     )
   })
 
